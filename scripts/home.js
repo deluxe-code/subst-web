f1574c96d5c979aacafbaaaf97c3588dceccb594
jacoballessio <jacob.allessio@gmail.com>
2020-05-04 21:02:03 -0700

    Update home.js

---
+++ scripts/home.js
@@ -0,0 +1,26 @@
+class Selector {
+
+    constructor(selectorOption){
+        try {
+            if()
+        }
+        catch(err) {
+            console.error("is not a valid type. Please provide a 'SelectorOption'");
+        }
+    }
+}
+
+class SelectorOption {
+    
+    constructor(){
+
+    }
+}
+
+class SelectorOptionNode{
+    
+}
+
+
+
+
