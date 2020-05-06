d988d778ca807ffb2bb819222f7deb795d3993cb
Ethan N <ethannikolas@gmail.com>
2020-05-05 18:46:24 -0700

    Funky Commit

--- scripts/home.js
+++ scripts/home.js
@@ -1,25 +1,3 @@
 // import Navbar from "./modules/navbar.js";
 // let navbar = new Navbar();
 // navbar.instantiate();
-class Selector {
-
-    constructor(selectorOption){
-        try {
-            if()
-        }
-        catch(err) {
-            console.error("is not a valid type. Please provide a 'SelectorOption'");
-        }
-    }
-}
-
-class SelectorOption {
-    
-    constructor(){
-
-    }
-}
-
-class SelectorOptionNode{
-    
-}
