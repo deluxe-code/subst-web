c1e798ae8bd6341e7a6cb0ca21d9e5f0517ae1b5
jacoballessio <jacob.allessio@gmail.com>
2020-05-05 18:37:37 -0700

    Nonya

---
+++ scripts/OptionSelector.js
@@ -0,0 +1,22 @@
+class Selector {
+
+    constructor(selectorOption){
+        try {
+            if(selectorOption)
+        }
+        catch(err) {
+            console.error("is not a valid type. Please provide a 'SelectorOption'");
+        }
+    }
+}
+
+class SelectorOption {
+    
+    constructor(){
+
+    }
+}
+
+class SelectorOptionNode{
+    
+}
