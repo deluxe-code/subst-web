e642bd3e8cd9e5063d621432c9fd4e521cd7d324
jacoballessio <jacob.allessio@gmail.com>
2020-05-05 18:43:52 -0700

    Re-added home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,25 @@
+// import Navbar from "./modules/navbar.js";
+// let navbar = new Navbar();
+// navbar.instantiate();
+class Selector {
 
+    constructor(selectorOption){
+        try {
+            if()
+        }
+        catch(err) {
+            console.error("is not a valid type. Please provide a 'SelectorOption'");
+        }
+    }
+}
 
+class SelectorOption {
+    
+    constructor(){
 
+    }
+}
 
-
+class SelectorOptionNode{
+    
+}
