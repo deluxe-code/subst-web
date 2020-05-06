e4e70629d9102b7b6031308a97828268e932c2a4
jacoballessio <jacob.allessio@gmail.com>
2020-05-05 18:41:21 -0700

    DELETE CHANGE

--- scripts/home.js
+++ scripts/home.js
@@ -1,25 +1,4 @@
-class Selector {
 
-    constructor(selectorOption){
-        try {
-            if()
-        }
-        catch(err) {
-            console.error("is not a valid type. Please provide a 'SelectorOption'");
-        }
-    }
-}
-
-class SelectorOption {
-    
-    constructor(){
-
-    }
-}
-
-class SelectorOptionNode{
-    
-}
 
 
 
