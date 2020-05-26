5e0c2f1b861fd16027d39e5fec21d0172bd4e1e2
Ethan N <ethannikolas@gmail.com>
2020-05-26 10:52:01 -0700

    Card UI Adjustments

--- scripts/modules/local_database.js
+++ scripts/modules/local_database.js
@@ -1,5 +1,17 @@
 export const drugs_directory = [
-    { name: "Marijuana", description: "Marijuana or weed is tubular", id: "drug_marijuana"},
-    { name: "Kratom", description: "Kratom or mitragyna speciosa is a...", id: "drug_kratom"},
-    { name: "Adderall", description: "Adderall or Amphetamine/d-amphetamine is a...", id: "drug_adderall"}
+    { 
+        name: "Marijuana", 
+        description: "Marijuana or weed is tubular", 
+        id: "drug_marijuana"
+    },
+    { 
+        name: "Kratom", 
+        description: "Kratom or mitragyna speciosa is a...", 
+        id: "drug_kratom"
+    },
+    {
+        name: "Adderall", 
+        description: "Adderall or Amphetamine/d-amphetamine is a...", 
+        id: "drug_adderall"
+    }
 ];
