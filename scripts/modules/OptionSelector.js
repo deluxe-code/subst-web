312a09415d4d6f7628a642a3d748785988ea9db1
jacoballessio <jacob.allessio@gmail.com>
2020-06-22 13:41:04 -0700

    Update OptionSelector.js

--- scripts/modules/OptionSelector.js
+++ scripts/modules/OptionSelector.js
@@ -153,7 +153,7 @@ class OptionSelectorAnimator {
         let targetPosition = -(this.mySelector.currentOption - 1) * this.mySelector.getOptionBoxHeight();
         let nextPosition = this.previousScrollPositionY;
         this.decelerationInterval = setInterval(smoothDeceleration, 1000 / 60);
-        let speed = 5;
+        let speed = 10;
         let decelerationRate = 50;
         function smoothDeceleration() {
 
