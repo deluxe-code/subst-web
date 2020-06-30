c2eda2c5932a0a6974e7c613b3928acfbfb1e020
jacoballessio <jacob.allessio@gmail.com>
2020-06-29 21:36:37 -0700

    DoseMonitoring Test

--- scripts/DoseMonitoring.js
+++ scripts/DoseMonitoring.js
@@ -24,13 +24,59 @@ class ScheduleItem {
     }
 }
 var scheduledItems = [];
+var swiped = false;
+const swipeTriggerDistance = 150;
 scheduledItems[0] = new ScheduleItem("Kratom", "8:30PM");
 scheduledItems[1] = new ScheduleItem("Krokodile", "8:30PM");
-scheduledItems[2] = new ScheduleItem("Krokodile", "8:30PM");
-window.addEventListener('load', (event) => { loadScheduledItems() });
+for (var i = 2; i < 15; i++) {
+    scheduledItems[i] = new ScheduleItem("Krokodile", "8:30PM");
+}
+window.addEventListener('load', (event) => { startUp() });
+function startUp() {
+    let touchOrigin;
+    let fingerPos;
+    let fingerPosY;
+    loadScheduledItems();
+    document.getElementById("bottomSection").style.touchAction = "none";
+    document.getElementById("bottomSection").addEventListener("touchstart", ev => {
+        fingerPos = ev.targetTouches[0];
+        fingerPosY = fingerPos.clientY;
+        touchOrigin = fingerPosY;
+        document.getElementById("bottomSection").style.transition = "transform 0s";
+    }, { passive: true });
+    document.getElementById("bottomSection").addEventListener("touchmove", ev => {
+        if (!swiped) {
+            fingerPos = ev.targetTouches[0];
+            fingerPosY = fingerPos.clientY;
+            dragList(fingerPosY, touchOrigin);
+        }
+    }, { passive: true });
+    document.getElementById("bottomSection").addEventListener("touchend", ev => {
+        if (!swiped) {
+            if ((fingerPosY - touchOrigin) < -swipeTriggerDistance) {
+                document.getElementById("bottomSection").style.transition = "transform 0.6s";
+                dragList(-document.getElementById("topSection").offsetHeight, 0);
+                document.getElementById("mainSection").style.overflow = "scroll";
+                document.getElementById("bottomSection").style.overflow = "scroll";
+                document.getElementById("bottomSection").removeEventListener("touchstart", ev => { }, false);
+                document.getElementById("bottomSection").removeEventListener("touchmove", ev => { }, false);
+                document.getElementById("bottomSection").removeEventListener("touchend", ev => { }, false);
+                swiped = true;
+            } else {
+                document.getElementById("bottomSection").style.transition = "transform 0.6s";
+                dragList(0, 0);
+            }
+        }
+    }, { passive: true });
+}
 function loadScheduledItems() {
     this.scheduledItems.forEach(element => {
         document.getElementById("bottomSection").appendChild(element.element);
     });
     document.getElementById("bottomSection").appendChild
+}
+function dragList(fingerPos, touchOrigin) {
+    relativeFingerPos = fingerPos - document.getElementById("topSection").offsetHeight;
+    fingerPosWithRespectToLocalPos = relativeFingerPos - (touchOrigin - document.getElementById("topSection").offsetHeight);
+    document.getElementById("bottomSection").style.transform = "translate(0px, " + (fingerPosWithRespectToLocalPos) + "px)";
 }
