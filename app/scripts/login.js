023689322301487f08ec80368d39ade77e60e858
Ethan N <ethannikolas@gmail.com>
2020-10-06 18:19:54 -0700

    Added basic signup & login functionality
    Also, a .SNIPPETS file has been created in the /dev folder, for storing custom-made  javascript solutions

--- app/scripts/login.js
+++ app/scripts/login.js
@@ -1,9 +1,15 @@
-// window.addEventListener("DOMContentLoaded", function() {
-//     document.getElementById("forgotPassword").addEventListener("click", () => {
-//         console.log("Time to send them a firebase auth forgot password tempate response");
-//     });
+// const { auth } = require("../modules/tools/firebase");
 
-//     document.getElementById("actionButton").addEventListener("click", () => {
-//         console.log("TIME TO AUTHENTICATE THE USER")
-//     });
-// });
+import { auth } from "../modules/tools/firebase.js";
+
+window.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("forgotPassword").addEventListener("click", () => {
+        console.log("Time to send them a firebase auth forgot password template response");
+    });
+
+    document.getElementById("actionButton").addEventListener("click", () => {
+        console.log("TIME TO AUTHENTICATE THE USER");
+        auth.tryLogin("ethannikolas@gmail.com","honeybunch9112");
+
+    });
+});
